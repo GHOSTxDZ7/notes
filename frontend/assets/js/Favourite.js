adfd4debba1aa9ef54993af4fb6938aaf10701ac
Darshan Zunjurde <darshanzunjurde@gmail.com>
2023-02-14 17:00:27 +0530

    1st

---
+++ frontend/assets/js/Favourite.js
@@ -0,0 +1,44 @@
+let favList = [];
+
+function setup() 
+{
+    let books = document.querySelectorAll(".btn");
+    for (let i = 0; i < books.length; i++)
+    {
+        books[i].onclick = function(e) {
+            addItem(e);
+        }
+    }
+}
+
+function addItem (e) {
+    let bookId = e.target.getAttribute("id");
+    if(!favList.find(element => element === bookId)){
+        let bookDiv = document.getElementById("book" + bookId);
+
+        let favDiv = document.createElement("div");
+        favDiv.setAttribute("id", "wish" + bookId);
+        favDiv.setAttribute("class", "book");
+        favDiv.setAttribute("style", "margin-bottom: 10px;")
+        favDiv.innerHTML += bookDiv.innerHTML;
+        let removeBtn = document.createElement("input");
+        removeBtn.setAttribute("id", "remove" + bookId);
+        removeBtn.setAttribute("type", "button");
+        removeBtn.setAttribute("value", "Remove");
+        removeBtn.setAttribute("class", "removebut");
+        removeBtn.onclick = () => removeItem(bookId);
+        favDiv.appendChild(removeBtn);
+
+        let aside = document.getElementById("favList");
+        aside.appendChild(favDiv);
+
+        favList.push(bookId); 
+    }
+}
+
+function removeItem(bookId) {
+    document.getElementById("wish" + bookId).remove();
+    favList = favList.filter(element => element !== bookId)
+}
+
+window.addEventListener("load", setup);
